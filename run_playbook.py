c944430c66daa32338e15527d1c51e86c53b16a6
Carl Soriano <114008663+carl-soriano@users.noreply.github.com>
2024-03-25 02:24:54 +0000

    finished version

--- run_playbook.py
+++ run_playbook.py
@@ -1,8 +1,9 @@
 """
 Author: Carl Soriano
-File_version: 1
+File_version: 1.1
 """
 import ansible_runner
+import requests
 
 def cmd_run_playbook(playbook_dir):
    
@@ -20,6 +21,24 @@ def main():
     playbook_path = "hello.yml"
     results = cmd_run_playbook(playbook_path)
     print_response(results)
+
+
+
+    expected_string1 = "Hello World from managedhost-app-1 !"
+    expected_string2 = "Hello World from managedhost-app-2 !"
+    expected_string3 = "Hello World from managedhost-app-3 !"
+
+    expected_strings = [expected_string1, expected_string2, expected_string3]
+
+    for i in range(3):
+        response = requests.get('http://0.0.0.0')
+        if response.text == expected_strings[i]:
+            print("Response matches the expected string:", response.text)
+        else:
+            print(f"Response doesn't match the expected string. Expected: {expected_strings[i]} but got: {response.text}")
+
+
+
 
 if __name__ == "__main__":
     main()
